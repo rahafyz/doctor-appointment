b80b7792c135bbe268c4ce030c8c581c1fc37fb8
rahafyz <r.fayaz1998@gmail.com>
2023-05-22 21:42:03 +0330

    AppointmentSlot service changed

--- doctor-appointment/src/main/java/com/blubank/doctorappointment/repository/AppointmentSlotRepository.java
+++ doctor-appointment/src/main/java/com/blubank/doctorappointment/repository/AppointmentSlotRepository.java
@@ -1,18 +1,16 @@
 package com.blubank.doctorappointment.repository;
 
-import com.blubank.doctorappointment.model.Appointment;
 import com.blubank.doctorappointment.model.AppointmentSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AppointmentSlotRepository extends JpaRepository<AppointmentSlot,Long> {
 
-    List<AppointmentSlot> findByAvailableTrue();
+    List<AppointmentSlot> findByIsAvailableTrue();
 
     List<AppointmentSlot> findByStartTimeBetween(LocalDateTime startTime,LocalDateTime endTime);
 }
