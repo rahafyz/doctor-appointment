7aad97eba8ea72e4bb2476811175a8a1aae2e9ad
rahafyz <r.fayaz1998@gmail.com>
2023-05-22 23:42:41 +0330

    appointmentSlot controller added

--- doctor-appointment/src/main/java/com/blubank/doctorappointment/controller/AppointmentSlotController.java
+++ doctor-appointment/src/main/java/com/blubank/doctorappointment/controller/AppointmentSlotController.java
@@ -6,7 +6,6 @@ import com.blubank.doctorappointment.service.AppointmentSlotService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
