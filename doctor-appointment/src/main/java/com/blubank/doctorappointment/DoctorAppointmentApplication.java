8b8f2e7e9f470bf1a4478fa9f2f86aa402df37b2
rahafyz <r.fayaz1998@gmail.com>
2023-05-24 02:20:00 +0330

    config

--- doctor-appointment/src/main/java/com/blubank/doctorappointment/DoctorAppointmentApplication.java
+++ doctor-appointment/src/main/java/com/blubank/doctorappointment/DoctorAppointmentApplication.java
@@ -3,10 +3,12 @@ package com.blubank.doctorappointment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableAdminServer
 @SpringBootApplication
 @EnableCaching
+@EnableJpaAuditing
 public class DoctorAppointmentApplication {
 
 	public static void main(String[] args) {
