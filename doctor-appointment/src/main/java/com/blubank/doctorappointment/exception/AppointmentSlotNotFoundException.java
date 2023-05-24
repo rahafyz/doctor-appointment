f69754f32978cb85f55cbf3c5dfd89eb98d1310d
rahafyz <r.fayaz1998@gmail.com>
2023-05-24 04:00:58 +0330

    messageSource added

--- doctor-appointment/src/main/java/com/blubank/doctorappointment/exception/AppointmentSlotNotFoundException.java
+++ doctor-appointment/src/main/java/com/blubank/doctorappointment/exception/AppointmentSlotNotFoundException.java
@@ -3,8 +3,8 @@ package com.blubank.doctorappointment.exception;
 import org.springframework.http.HttpStatus;
 
 public class AppointmentSlotNotFoundException extends CustomException{
-    private final static String desc = "blubank.appointment.slot.not.found.exception";
+    private static final String DESC = "blubank.appointment.slot.not.found.exception";
     public AppointmentSlotNotFoundException() {
-        super(desc, HttpStatus.NOT_FOUND);
+        super(DESC, HttpStatus.NOT_FOUND);
     }
 }
