ef082eaff5c3f87a841cdd757f38f039829a2c7d
rahafyz <r.fayaz1998@gmail.com>
2023-05-24 04:02:05 +0330

    messageSource added

--- doctor-appointment/src/main/java/com/blubank/doctorappointment/exception/DuplicatePatientException.java
+++ doctor-appointment/src/main/java/com/blubank/doctorappointment/exception/DuplicatePatientException.java
@@ -3,8 +3,8 @@ package com.blubank.doctorappointment.exception;
 import org.springframework.http.HttpStatus;
 
 public class DuplicatePatientException extends CustomException{
-    private final static String desc = "blubank.patient.duplicate.exception";
+    private static final String DESC = "blubank.patient.duplicate.exception";
     public DuplicatePatientException() {
-        super(desc,HttpStatus.NOT_ACCEPTABLE);
+        super(DESC,HttpStatus.NOT_ACCEPTABLE);
     }
 }
