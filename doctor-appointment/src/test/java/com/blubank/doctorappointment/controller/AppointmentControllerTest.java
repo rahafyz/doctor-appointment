8e8b8d30e9b8fc8f9ce7dcc5f293ff1be296e2fc
rahafyz <r.fayaz1998@gmail.com>
2023-05-26 20:16:32 +0330

    integration tests

---
+++ doctor-appointment/src/test/java/com/blubank/doctorappointment/controller/AppointmentControllerTest.java
@@ -0,0 +1,4 @@
+package com.blubank.doctorappointment.controller;
+
+public class AppointmentControllerTest {
+}
--- doctor-appointment/src/test/java/com/blubank/doctorappointment/controller/PatientControllerTest.java
+++ doctor-appointment/src/test/java/com/blubank/doctorappointment/controller/PatientControllerTest.java
@@ -1,4 +1,95 @@
 package com.blubank.doctorappointment.controller;
 
-public class PatientControllerTest {
+import com.blubank.doctorappointment.exception.GlobalExceptionHandling;
+import com.blubank.doctorappointment.repository.AppointmentRepository;
+import com.blubank.doctorappointment.repository.AppointmentSlotRepository;
+import com.blubank.doctorappointment.repository.DoctorRepository;
+import com.blubank.doctorappointment.repository.PatientRepository;
+import com.blubank.doctorappointment.service.PatientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.format.DateTimeFormatter;
+
+import static com.blubank.doctorappointment.util.AppointmentData.appointment;
+import static com.blubank.doctorappointment.util.AppointmentSlotData.appointmentSlot;
+import static com.blubank.doctorappointment.util.DoctorData.doctor;
+import static com.blubank.doctorappointment.util.PatientData.patientData;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PatientControllerTest {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private AppointmentSlotRepository appointmentSlotRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    GlobalExceptionHandling exceptionHandler;
+    private MockMvc mockMvc;
+
+    private final static String PHONE_NUMBER = "09121234567";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @BeforeEach
+    void setup() {
+        PatientController controller = new PatientController(patientService);
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setMessageConverters(jacksonMessageConverter)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setControllerAdvice(exceptionHandler)
+                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    void getOpenAppointments() throws Exception {
+        doctorRepository.save(doctor());
+        appointmentSlotRepository.save(appointmentSlot());
+        patientRepository.save(patientData());
+        appointmentRepository.save(appointment());
+
+        mockMvc.perform(get("/api/v1/patient/appointments")
+                        .param("phoneNumber",PHONE_NUMBER))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].appointmentSlot.startTime").value(appointmentSlot().getStartTime().format(formatter)))
+                .andExpect(jsonPath("$[0].appointmentSlot.endTime").value(appointmentSlot().getEndTime().format(formatter)));
+
+    }
 }
